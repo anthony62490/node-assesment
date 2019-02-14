78afc7fab269e26f84dc142bf169c3c04a0846bb
anthony62490 <anthonyjmagno@gmail.com>
2019-02-13 18:49:35 -0600

    addTrip function complete

--- server/controller.js
+++ server/controller.js
@@ -10,7 +10,8 @@ var DB = {
       est_fare_low: 65,
       est_fare_high: 75,
       assigned_driver: 0,
-      assigned_vehicle: 0
+      assigned_vehicle: 0,
+      requested_music: 2
     }
   ],
   drivers: [
@@ -178,7 +179,7 @@ const  getAllMusic = (req, res, next) => {
 // POST endpoints
 //
 
-const  addTrip = (req, res, next) => {
+const addTrip = (req, res, next) => {
   //expects an object containing an
   //eta:              String  24hour time of arrival
   //start_location:   String  starting address obtained from Maps. Lines delimited by a '%'
@@ -189,6 +190,7 @@ const  addTrip = (req, res, next) => {
   //est_fare_high:    Integer estimated high fare,,
   //assigned_driver:  Integer id of assigned driver,
   //assigned_vehicle: Integer id of assigned vehicle,
+  //requested_music:  Integer id of requested music
 
   let foundKeys = Object.keys(req.body);
   let expectedKeys = [
@@ -200,17 +202,76 @@ const  addTrip = (req, res, next) => {
     'est_fare_low',
     'est_fare_high',
     'assigned_driver',
-    'assigned_vehicle'];
-  
+    'assigned_vehicle',
+    'requested_music'];
+
   //checking that all expected keys are accounted for. missingKeys === [] if all keys are present
   missingKeys = expectedKeys.filter((e) => !foundKeys.includes(e));
-  if(missingKeys.length) {
+  if (missingKeys.length) {
     errorStr = missingKeys.join(', ');
     res.status(400).send(`Bad Request. Missing values for ${errorStr}`);
+    return -1;
   }
 
+  //checking that provided values match required format
 
-  console.log(req.body);
+  //tests that eta is a valid time in 24hour format
+  if (/([01]\d|2[0-3]):[0-5][0-9]/.test(req.body.eta) === false) {
+    res.status(400).send('Malformed ETA. ETA should be a 24hour time with leading zeroes')
+    return -1;
+  };
+  //checking that addresses match required format
+  if (/[a-zA-Z0-9\s.',]+[%][a-zA-Z0-9\s.',]+/.test(req.body.start_location) === false ||
+    /[a-zA-Z0-9\s.',]+[%][a-zA-Z0-9\s.',]+/.test(req.body.end_location) === false) {
+    //this probably doesn't work for a large number of cases, but I figure that accurate error handling is beyond the scope of the project
+    res.status(400).send('Malformed Address. Address should be a string with lines delimited by "%"')
+    return -1;
+  };
+  //checking that estimated fares are integers and that they are in the right order
+  if (!Number.isInteger(req.body.est_fare_low) || !Number.isInteger(req.body.est_fare_high)) {
+    res.status(400).send('Malformed fare estimations. Fares must be integers')
+    return -1;
+  }
+  if (req.body.est_fare_high < req.body.est_fare_low) {
+    res.status(400).send('Bad fare estimations. Low fare must be the smaller value')
+    return -1;
+  }
+  //checking that assigned driver exists
+  if (!Number.isInteger(req.body.assigned_driver) ||
+    !retrieveDBObjByID(Number(req.body.assigned_driver), "drivers")) {
+    res.status(404).send('Requested driver id not found')
+    return -1;
+  }
+  //checking that assigned vehicle exists
+  if (!Number.isInteger(req.body.assigned_vehicle) ||
+    !retrieveDBObjByID(Number(req.body.assigned_vehicle), "cars")) {
+    res.status(404).send('Requested vehicle id not found')
+    return -1;
+  }
+  //checking that music exists
+  if (!Number.isInteger(req.body.assigned_vehicle) ||
+    !retrieveDBObjByID(Number(req.body.requested_music), "music")) {
+    res.status(404).send('Requested music id not found')
+    return -1;
+  }
+
+  //if there hasn't been a 40X error by now, then the data should be good. Add it to DB
+  let newId = DB.trips[DB.trips.length - 1].id + 1;
+  DB.trips.push({
+      //assign new id one higher than last existing id
+      id: newId,
+      eta: req.body.eta,
+      start_location: req.body.start_location,
+      end_location: req.body.end_location,
+      payment: req.body.payment,
+      customer_note: req.body.customer_note,
+      est_fare_low: req.body.est_fare_high,
+      est_fare_high: req.body.est_fare_low,
+      assigned_driver: req.body.assigned_driver,
+      assigned_vehicle: req.body.assigned_vehicle,
+      requested_music: req.body.requested_music
+  });
+  res.status(200).send(newId + '');
 }
 
 const  addDriver = (req, res, next) => {
